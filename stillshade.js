65207ea55d1da33593ab12c3bd010d3e4918c46e
George Ramirez <gfrayramirez@gmail.com>
2022-11-28 23:30:00 -0500

    Projecting, but no shadows yet

--- stillshade.js
+++ stillshade.js
@@ -48,12 +48,16 @@ const StillShade = (function () {
                 highp vec4 resultColor = vec4(1.0, 1.0, 1.0, 1.0);
                 //tyref: funglshadows
                 vec3 projectedTexcoord = v_projectedTexcoord.xyz / (v_projectedTexcoord.w * 1.0);
+                //from the shadow shader: gl_Position[2] * gl_Position[3] * 0.01;
+                float currentDepth = v_projectedTexcoord.z * 0.11 * 0.333;//// * v_projectedTexcoord.w * 0.001;
                 bool inRange = 
                       projectedTexcoord.x >= 0.0 &&
                       projectedTexcoord.x <= 1.0 &&
                       projectedTexcoord.y >= 0.0 &&
                       projectedTexcoord.y <= 1.0;
-                vec4 projectedTexColor = vec4(texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).rrr * 1.0, 1);//vec4(0.2, 0.2, texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).r * 0.37, 1);////vec4(texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).rrr * 1.0, 1);//// vec2(vTextureCoord[0], vTextureCoord[1]));//
+                //vec4 projectedTexColor = vec4(texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).rrr * 1.0, 1);//vec4(0.2, 0.2, texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).r * 0.37, 1);////vec4(texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).rrr * 1.0, 1);//// vec2(vTextureCoord[0], vTextureCoord[1]));//
+                float projectedDepth = texture2D(uProjectedTexture, projectedTexcoord.xy).r;
+                float shadowLight = (inRange && projectedDepth <= currentDepth) ? 0.0 : 1.0;  
 
                 highp vec4 texelColor = texture2D(uSampler, vec2(vTextureCoord[0], vTextureCoord[1]));// vTextureCoord);
 
@@ -62,8 +66,13 @@ const StillShade = (function () {
                 highp float pointlight = max(abs(dot(normWorld, surfaceToLightDirection)), 0.0); ////max(dot(vNormWorld, vPosToLight), abs(dot(vNormWorld, vPosToLight))); ////max(dot(vNormWorld, vPosToLight), dot(vNormWorld, vPosToLight));
 
                 resultColor = vec4(texelColor.rgb * vLighting, texelColor.a * 1.0);//texelColor.a * 1.0
-                resultColor.rgb *= (1.0 + 1.0 * pointlight * vec3(0.4, 0.85, 1.0));
+                resultColor.rgb *= shadowLight *(1.0 + shadowLight * pointlight * vec3(0.4, 0.85, 1.0));//(1.0 + 1.0 * pointlight * vec3(0.4, 0.85, 1.0));
 
+                //testval
+                ////resultColor = vec4(texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).rrr * 0.333, 1);//
+                ////resultColor = vec4(currentDepth * 1.0,currentDepth * 1.0, currentDepth * 1.0,  1);//
+                resultColor = vec4(currentDepth * 2.0 - texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).r * 1.0, currentDepth * 2.0 - texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).r * 1.0, currentDepth * 2.0 - texture2D(uProjectedTexture, projectedTexcoord.xy * 1.0).r * 1.0, 1);
+                
                 // Just add in the specular
                 highp vec3 surfaceToViewDirection = normalize(vPosToCam);
                 highp vec3 halfVector = normalize(surfaceToLightDirection + surfaceToViewDirection);
@@ -82,8 +91,8 @@ const StillShade = (function () {
                     resultColor = vec4(ceil(resultColor[0] * ucelStep) / ucelStep, ceil(resultColor[1] * ucelStep) / ucelStep, ceil(resultColor[2] * ucelStep) / ucelStep, resultColor[3]);
                 }
                 ////vec4 texColor = texture2D(u_texture, v_texcoord) * u_colorMult;
-                float projectedAmount = inRange ? 1.0 : 0.0;
-                resultColor = mix(resultColor, projectedTexColor, projectedAmount);
+                ////float projectedAmount = inRange ? 1.0 : 0.0;
+                ////resultColor = mix(resultColor, projectedTexColor, projectedAmount);
                 gl_FragColor = resultColor;
             }
         `;
@@ -105,6 +114,7 @@ const StillShade = (function () {
             uniform vec3 uLightDirection;
 
             uniform mat4 uOverTextureMatrix;
+            uniform mat4 uGlobalModInv;//is this really needed
 
             varying highp vec3 vTextureCoord;
             varying highp vec3 vLighting;
@@ -158,9 +168,10 @@ const StillShade = (function () {
                 // and pass it to the fragment shader
                 vPosToCam = vec3(uProjectionMatrix[3][0], uProjectionMatrix[3][1], uProjectionMatrix[3][2]) - pointWorldPos;//uProjectionMatrix[12], uProjectionMatrix[13], uProjectionMatrix[14]
 
-                v_projectedTexcoord = uOverTextureMatrix * (worldSpaceMat * aVertexPosition);//(worldSpaceMat * aVertexPosition);//uOverTextureMatrix * (worldSpaceMat * aVertexPosition);// uOverTextureMatrix * (worldSpaceMat * aVertexPosition);//add worldSpaceMat or no?
-                v_projectedTexcoord = (worldSpaceMat * aVertexPosition);//
+                v_projectedTexcoord = uOverTextureMatrix * uGlobalModInv * (worldSpaceMat * aVertexPosition);//(worldSpaceMat * aVertexPosition);//uOverTextureMatrix * (worldSpaceMat * aVertexPosition);// uOverTextureMatrix * (worldSpaceMat * aVertexPosition);//add worldSpaceMat or no?
+                ////v_projectedTexcoord = uGlobalModInv * uProjectionMatrix * (worldSpaceMat * aVertexPosition);//
                 //v_projectedTexcoord[2] = 0.999;//??
+                v_projectedTexcoord[2] = v_projectedTexcoord[2] * v_projectedTexcoord[3] * 0.01;
             }
         `;
 
@@ -197,58 +208,75 @@ const StillShade = (function () {
 
                 textureMatrix = mat4.create();
 
-                ////attempt 1     
-                //var tempmat2 = mat4.create();
-                //mat4.rotate(tempmat2,  // destination matrix
-                //    tempmat2,  // matrix to rotate
-                //    -.452,//.7,   // amount to rotate in radians
-                //    [1, 0, 0]); //console.log(Date.now() * .01);
-                //mat4.rotate(tempmat2,  // destination matrix
-                //    tempmat2,  // matrix to rotate
-                //    -1.752,//.7,   // amount to rotate in radians
-                //    [0, 1, 0]);
-                //mat4.translate(tempmat2,     // destination matrix
-                //    tempmat2,     // matrix to translate
-                //    [-6.0, 4.15, -1.62]);//[-11.0, 4.0, 0.0]);//[-5.5, 4.0, 0.0]);  62
-                //mat4.scale(tempmat2, tempmat2, [0.275, 0.275, 0.275]);
-                ////mat4.invert(tempmat2, tempmat2);
+                //attempt 3
+                var tempmat3 = mat4.create();
+                //mat4.rotate(textureMatrix,  // destination matrix
+                //    textureMatrix,  // matrix to rotate
+                //    -3.14159,//.7,   // amount to rotate in radians
+                //    [1, 0, 0]);
+                ////mat4.rotate(textureMatrix,  // destination matrix
+                ////    textureMatrix,  // matrix to rotate
+                ////    3.14159,//.7,   // amount to rotate in radians
+                ////    [0, 0, 1]);
+                //mat4.translate(textureMatrix,     // destination matrix
+                //    textureMatrix,     // matrix to translate
+                //    [-46.0, 0.0, -28.0]);//
 
-                //var temptmat = mat4.create();
-                //mat4.invert(temptmat, gmod);
-                //mat4.multiply(textureMatrix, tempmat2, temptmat);
-                //if (gproj) {
-                //    var tempproj = mat4.create();
-                //    ////mat4.scale(tempproj, gproj, [0.1, 0.1, 0.1]);
-                //    mat4.multiply(textureMatrix, gproj, textureMatrix);
-                //}
-                ////mat4.scale(textureMatrix, textureMatrix, [0.5, 0.5, 0.5]);
-                ////end of attempt 1
-
-                //attempt 2     
-                var tempmat2 = mat4.create();
-                mat4.rotate(tempmat2,  // destination matrix
-                    tempmat2,  // matrix to rotate
-                    .252,//(Date.now() * .001),//-.452,//.7,   // amount to rotate in radians
-                    [1, 0, 0]); //console.log(Date.now() * .01);
-                mat4.rotate(tempmat2,  // destination matrix
-                    tempmat2,  // matrix to rotate
-                    -1.752,//.7,   // amount to rotate in radians
-                    [0, 1, 0]);
-                mat4.translate(tempmat2,     // destination matrix
-                    tempmat2,     // matrix to translate
-                    [-44.0, -8.0, 0.0]);//[-11.0, -4.0, 0.0]);
-                //mat4.invert(tempmat2, tempmat2);
-
-                var temptmat = mat4.create();
-                mat4.invert(temptmat, gmod);
-                mat4.multiply(textureMatrix, tempmat2, temptmat);
-                if (gproj) {
+                /*if (gproj && true) {
                     var tempproj = mat4.create();
                     ////mat4.scale(tempproj, gproj, [0.1, 0.1, 0.1]);
+                    mat4.invert(tempproj, gproj);
+                    mat4.multiply(textureMatrix, textureMatrix, gproj);
+                }
+
+                ////mat4.scale(tempmat3, tempmat3, [24.0, 0.1, 24.0]);
+                mat4.rotate(tempmat3,  // destination matrix
+                    tempmat3,  // matrix to rotate
+                    .052,//.252,//(Date.now() * .001),//-.452,//.7,   // amount to rotate in radians
+                    [1, 0, 0]); //console.log(Date.now() * .01);
+                mat4.rotate(tempmat3,  // destination matrix
+                    tempmat3,  // matrix to rotate
+                    -1.5707,//.7,   // amount to rotate in radians
+                    [0, 1, 0]);
+                mat4.translate(tempmat3,     // destination matrix
+                    tempmat3,     // matrix to translate
+                    [-44.0, -8.0, 0.0]);
+
+                mat4.invert(tempmat3, tempmat3);
+                mat4.multiply(textureMatrix, textureMatrix, tempmat3);
+                */
+
+                //attempt 4
+                mat4.rotate(tempmat3,  // destination matrix
+                    tempmat3,  // matrix to rotate
+                    .052,//.252,//(Date.now() * .001),//-.452,//.7,   // amount to rotate in radians
+                    [1, 0, 0]); //console.log(Date.now() * .01);
+                mat4.rotate(tempmat3,  // destination matrix
+                    tempmat3,  // matrix to rotate
+                    -1.752,//-1.5707,//.7,   // amount to rotate in radians
+                    [0, 1, 0]);
+                mat4.translate(tempmat3,     // destination matrix
+                    tempmat3,     // matrix to translate
+                    [-44.0, -8.0, 0.0]);
+                mat4.multiply(textureMatrix, textureMatrix, tempmat3);
+                if (gproj && true) {
                     mat4.multiply(textureMatrix, gproj, textureMatrix);
                 }
-                //mat4.scale(textureMatrix, textureMatrix, [0.5, 0.5, 0.5]);
-                //end of attempt 2
+
+                tempmat3 = mat4.create();
+                //mat4.translate(tempmat3,     // destination matrix
+                //    tempmat3,     // matrix to translate
+                //    [0.666, 0.501, 0.666]);
+                mat4.translate(tempmat3,     // destination matrix
+                    tempmat3,     // matrix to translate
+                    [0.50, 0.50, 0.50]);
+                mat4.scale(tempmat3, tempmat3, [0.5, 0.5, 0.5]);
+                mat4.multiply(textureMatrix, tempmat3, textureMatrix);
+
+                //mat4.translate(textureMatrix,     // destination matrix
+                //    textureMatrix,     // matrix to translate
+                //    [-140.5, 25.5, -60.5]);
+                //mat4.invert(textureMatrix, textureMatrix);
 
                 gl.uniformMatrix4fv(
                     attr.loc,
@@ -262,6 +290,23 @@ const StillShade = (function () {
             loc: wgl.getUniformLocation(globalMainProgramInfo.program, 'uProjectedTexture'),
             frameset: function (attr, gl) {
                 gl.uniform1i(attr.loc, 1);
+            }
+        });
+        customUniforms.push({
+            name: 'uGlobalModInv',
+            loc: wgl.getUniformLocation(globalMainProgramInfo.program, 'uGlobalModInv'),
+            frameset: function (attr, gl) {
+
+                var modinv = mat4.create();
+                mat4.invert(modinv, gmod);////just temp out
+                //mat4.translate(modinv,     // destination matrix
+                //    modinv,     // matrix to translate
+                //    [0.5, 0.5, 0.5]);
+                //mat4.scale(modinv, modinv, [0.5, 0.5, 0.5]);
+                gl.uniformMatrix4fv(
+                    attr.loc,
+                    false,
+                    modinv);
             }
         });
 
